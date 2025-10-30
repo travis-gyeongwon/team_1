28ec688ef0da19d2264b91f687cb7cce01780dc1
usaer <usaer@DESKTOP-74BHFVD>
2025-10-30 17:02:05 +0900

    로그인 버튼 눌렀을 때 작동 안함 수정

--- kiosk_prj/src/kiosk/admin/event/AdminLoginEvent.java
+++ kiosk_prj/src/kiosk/admin/event/AdminLoginEvent.java
@@ -13,20 +13,16 @@ import kiosk.admin.view.AdminMainDesign;
 
 public class AdminLoginEvent extends WindowAdapter implements ActionListener {
 	private AdminLoginDesign ald;
+	private String id;
+	private String pw;
 	
 	public AdminLoginEvent(AdminLoginDesign ald) {
 		this.ald = ald;
 	}
 
-	private void confirmLogin() {
-		String id = ald.getJtfId().getText().trim();
-		String pw = new String(ald.getJtfPass().getPassword()).trim();
-		if(id.isEmpty() || pw.isEmpty()) {
-			ald.getJlError().setText("아이디 또는 비밀번호를 입력해주세요.");
-			if(id.isEmpty())
-				ald.getJtfId().requestFocus();
-			return;
-		}
+	private void confirmLogin(boolean login) {
+		if(login) return;
+
 		ald.getJlError().setText("");
 		AdminLoginService als = new AdminLoginService();
 		if(als.searchLogin(id, pw) != null) {
@@ -35,16 +31,30 @@ public class AdminLoginEvent extends WindowAdapter implements ActionListener {
 		}else {
 			JOptionPane.showMessageDialog(ald, "아이디/비밀번호를 확인해주세요");
 		}
+		
+	}
+	
+	private boolean checkBlank(String id, String pw) {
+		boolean flag = false;
+		
+		if(id.isEmpty() || pw.isEmpty()) {
+			ald.getJlError().setText("아이디 또는 비밀번호를 입력해주세요.");
+			if(id.isEmpty()) {
+				ald.getJtfId().requestFocus();
+			}else {
+				ald.getJtfPass().requestFocus();
+			}
+			flag = true;
+		}
+		return flag;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
-		if(ae.getSource() == ald.getJtfId()) {
-			ald.getJtfPass().requestFocus();
-		}
-		if(ae.getSource() == ald.getJtfPass()) {
-			confirmLogin();
-		}
+		id = ald.getJtfId().getText().trim();
+		pw = new String(ald.getJtfPass().getPassword()).trim();
+		
+		confirmLogin(checkBlank(id, pw));
 	}
 
 	@Override
